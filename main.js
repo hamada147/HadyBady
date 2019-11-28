dd2aac210bfafb7d6212f91e4995fa847242d11b
Ahmed Moussa <moussa.ahmed95@gmail.com>
2019-11-28 13:33:30 +0200

    Fixed Syntax

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 function hadyBady() {
-  var words = ["hady", "bady", "cromp", "zabady", "shalo", "we 7ato", "kolo", "3lady", "ya katkot", "ro7", "souq", "hat", "elbida", "men", "elsandok", "aowa", "takolha", "tet2a", "temot", "ya katkot"];
-  for (let i = 0, size = words.length; i < size; ++i) {
-	if (i % 2 == 0) { // even
-		console.log(words[i], "YES");
-	} else { // odd
-		console.log(words[i], "NO");
+	var words = ["hady", "bady", "cromp", "zabady", "shalo", "we 7ato", "kolo", "3lady", "ya katkot", "ro7", "souq", "hat", "elbida", "men", "elsandok", "aowa", "takolha", "tet2a", "temot", "ya katkot"];
+	for (let i = 0, size = words.length; i < size; ++i) {
+		if (i % 2 == 0) { // even
+			console.log(words[i], "YES");
+		} else { // odd
+			console.log(words[i], "NO");
+		}
 	}
 }
